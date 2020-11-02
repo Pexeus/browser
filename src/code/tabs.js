9ac5d1f370ce9bd53b7c2558ef9396443e654c30
unknown <libe>
2020-11-02 23:43:20 +0100

    tabs

--- src/code/tabs.js
+++ src/code/tabs.js
@@ -1,6 +1,9 @@
+const view = require("./webviews")
 const tabContainer = vie.get("#tabs")
 
 function newTab(url) {
+    view.new(url)
+
     const newTab = vie.get("#newTab")
     const tab = vie.new("div", ".tab")
     tab.id = url
@@ -10,6 +13,10 @@ function newTab(url) {
     const closeTab = vie.new("img", "#closeTab")
     closeTab.src = "./img/close.png"
 
+    tab.addEventListener("click", function() {
+        switchTo(event.target)
+    })
+
     tab.appendChild(tabName)
     tab.appendChild(closeTab)
     tabContainer.appendChild(tab)
@@ -17,4 +24,16 @@ function newTab(url) {
     tabContainer.appendChild(newTab)
 }
 
-newTab("google.com")
+function switchTo(tab) {
+    while(tab.tagName != "DIV") {
+        tab = tab.parentElement
+    }
+
+    tab.id = "active"
+}
+
+vie.get("#newTab").addEventListener("click", () => {
+    newTab("https://www.google.com/")
+})
+
+newTab("https://www.google.com/")
